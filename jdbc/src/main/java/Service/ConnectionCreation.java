ceb88ae99785d39d582293a400f66646f8f5f0e5
aniket234121 <aniketchauhan329@gmail.com>
2025-06-15 18:08:24 +0530

    --added-- preparedStatement ui handler

--- jdbc/src/main/java/Service/ConnectionCreation.java
+++ jdbc/src/main/java/Service/ConnectionCreation.java
@@ -1,5 +1,6 @@
 package Service;
 
+import CRUDtoDB.PreparedStatementCrud;
 import CRUDtoDB.StatementCRUD;
 
 import java.sql.Connection;
@@ -53,7 +54,51 @@ public class ConnectionCreation {
         }
         return 0;
     }
-
+    public static int handlePreparedStatement(Connection con, Scanner sc) throws Exception {
+        try (PreparedStatementCrud pstmtCrud = new PreparedStatementCrud()) {
+            int option = 0;
+            while (option != 5) {
+                System.out.println("=====Prepared statement arena======");
+                System.out.println("1.Create user");
+                System.out.println("2.Read user");
+                System.out.println("3.Update user");
+                System.out.println("4.Delete user");
+                System.out.println("5.Return to Main Menu");
+                option = sc.nextInt();
+                switch (option) {
+                    case 1:
+                        System.out.println("enter name of user:- ");
+                        String name = sc.next();
+                        System.out.println("enter email of user:- ");
+                        String email = sc.next();
+                        pstmtCrud.saveToDB(name, email, con);
+                        break;
+                    case 2:
+                        pstmtCrud.getFromDB(con);
+                        break;
+                    case 3:
+                        System.out.println("Id of User to Update");
+                        int id = sc.nextInt();
+                        System.out.println("enter new Name");
+                        String newname = sc.next();
+                        System.out.println("enter new Email");
+                        String newemail = sc.next();
+                        pstmtCrud.updateUser(id, newname, newemail, con);
+                        break;
+                    case 4:
+                        System.out.println("enter user id to delete");
+                        id = sc.nextInt();
+                        pstmtCrud.removeFromDB(id, con);
+                        break;
+                    case 5:
+                        System.out.println("Returning to main menu...");
+                        System.out.println("============================");
+                        return 0;
+                }
+            }
+        }
+        return 0;
+    }
     public void start() {
         String url = "jdbc:mysql://localhost:3306/testJDBC";
         String userName = "newuser";
@@ -78,6 +123,7 @@ public class ConnectionCreation {
                         handleStatement(con, sc); // Scanner passed here
                         break;
                     case 2:
+                        handlePreparedStatement(con,sc);
                         break;
                     case 3:
                         break;
